5a0eb770b98b8b7909479a2ebf143429efe90320
aguilaradry <adriana.aguilar@est.iudigital.edu.co>
2025-04-03 21:53:44 -0500

    Update cleaning.py

--- scripts/cleaning.py
+++ scripts/cleaning.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import pandas as pd
+from airflow.utils.log.logging_mixin import LoggingMixin
 #Pistas
 # En este paso la idea sería quitar duplicados, manejar nulos, pero como no es el objetivo, 
 # Vamos a hacer una copia espejo de los datos, simulando que los datos ya están limpios.
@@ -10,6 +11,31 @@ import sqlite3
 # 3: Guardar los cambios y cerrar la conexión
 # 4: Usa print() para mostrar el estado del proceso
 
+log = LoggingMixin().log
+
 def cleaning_data():
     db_path = '/opt/airflow/dags/data/ecommerce.db'
-    #TODO
+
+    conn = sqlite3.connect(db_path)
+
+    # Diccionario: tabla bronze → tabla silver
+    tables = {
+        'bronze_olist_orders': 'silver_olist_orders',
+        'bronze_olist_order_payments': 'silver_olist_order_payments',
+        'bronze_olist_customers': 'silver_olist_customers'
+    }
+
+    for bronze_table, silver_table in tables.items():
+        try:
+            df = pd.read_sql_query(f"SELECT * FROM {bronze_table}", conn)
+
+            log.info(f"Datos cargados desde {bronze_table}: {df.shape[0]} filas.")
+
+            df.to_sql(silver_table, conn, if_exists='replace', index=False)
+            log.info(f"Tabla '{silver_table}' creada a partir de '{bronze_table}'.")
+        
+        except Exception as e:
+            log.error(f"Error al copiar {bronze_table} a {silver_table}: {e}")
+
+    conn.close()
+    log.info("Todas las tablas han sido copiadas de bronze a silver.")
